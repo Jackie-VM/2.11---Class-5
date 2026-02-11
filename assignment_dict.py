1b16161ae5cf524cb32c818f54d759f80bb77876
Jacqueline Means <JackieVM@udel.edu>
2026-02-11 11:21:46 -0500

    note...

---
+++ assignment_dict.py
@@ -0,0 +1,8 @@
+assignment= {
+"title" : "Homework 1: Intro to Database Design",
+"description": "Create an ER Model",
+"due date": "2026-01-10",
+"available": False,
+"questions": []
+}
+print(assignment)
